e1e800dcfc7766d67c2f3998479e56a6e22c790e
CryPara <cry.ca89@gmail.com>
2015-01-21 14:02:07 +0100

    aggiunti file

---
+++ code/ping.h
@@ -0,0 +1,71 @@
+/*function that pings all address within the database*/
+int ping()
+{
+    char ping[PING_MAX_LENGTH];
+    MYSQL_RES *result;
+    sprintf(query_select_domain_part,"SELECT domain_part FROM Domain_part;");
+    if (mysql_query(&mysql,query_select_domain_part)==0)
+    {
+        result = mysql_store_result(&mysql);
+        if(result==NULL)
+        {
+            printf("\n Error: result = null \n");
+        }
+        else
+        {	
+            MYSQL_ROW row;
+	    /*row contains domain_part */
+            while ((row = mysql_fetch_row(result)))
+            {
+                sprintf(ping,"ping -c 1 -w 5 www.%s",row[0]); /* -c: stop after sending count , -w: specify  a  timeout in seconds */
+	
+                if(system (ping)!=0) 
+		{   /*error occurred */
+                    printf("\n %s: NO PONG! \n",row[0]);
+                    sprintf(query_insert_flag_domain_part_A,"UPDATE Address SET invalid_domain='1' WHERE domain_part='%s';",row[0]);
+		    sprintf(query_insert_flag_domain_part_Dp,"UPDATE Domain_part SET invalid_domain='1' WHERE domain_part='%s';",row[0]);
+		    
+		    
+		    /*set invalid_domain=1 if server doesn't respond or doesn't exist */
+                    if((mysql_query(&mysql,query_insert_flag_domain_part_A)==0) && (mysql_query(&mysql,query_insert_flag_domain_part_Dp)==0))
+                    {
+                        printf("\n %s: invalid domain. Flag inserted! \n",row[0]);
+                    }
+                    else
+                    {
+                        printf("\n query error \n");
+                    }
+                }
+                else
+		  /*server responds */
+                    printf("\n%s: PONG! \n",row[0]);
+            }
+        }
+       
+        count_ping_no_respons();
+        return 1;
+    }
+    else
+    {
+        printf("\n select query error \n");
+       
+    }
+    return 0;
+}
+
+/*counts the number of servers that do not respond after being pinged */
+void count_ping_no_respons(){
+  MYSQL_RES *result;
+  sprintf(query_count_flag_domain_part, "SELECT count(domain_part) FROM Domain_part WHERE invalid_domain='1';");
+  if(mysql_query(&mysql,query_count_flag_domain_part)==0){
+    result = mysql_store_result(&mysql);
+        if (result == NULL)
+            printf("\n Error: result = null\n");
+    else {
+        MYSQL_ROW row = mysql_fetch_row(result);
+	  printf(" number of servers that do not respond %s\n",row[0]);
+}
+  }
+  else 
+    printf("\n query error \n");
+}
---
+++ readme.txt
@@ -0,0 +1,25 @@
+Università degli studi di Catania
+
+MailParser
+
+Author:
+
+-Roberta Maccarone
+-Cristina Parasiliti
+-Safarally Dario
+
+Menu:
+
+1. Import email addresses from files
+2. Insert an email address
+3. Delete an email address
+4. Delete email addresses from file
+5. Search an email
+6. Export the database to .txt files
+7. PING: Check which mail servers respond
+8. Exit
+
+
+Compile:
+1)if you use Ubuntu, any version, you can use the instruction "make name_file".
+2)for the other O.S you can use "gcc -o nameobject $(mysql_config --cflags) nameprogram.c $(mysql_config --libs)
